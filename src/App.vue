06c959e5412220468c26c447296c8b4cea653de8
shihongxins <shihongxins@163.com>
2022-07-07 17:16:21 +0800

    feat: 使用预定义 document 解耦文档版

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-  import CKEditorClassicVue from './components/CKEditorClassic/CKEditorClassic.vue';
+  // import CKEditorClassicVue from './components/CKEditorClassic/CKEditorClassic.vue';
+  // 无法共存
+  import CKEditorDocumentVue from './components/CKEditorDocument/CKEditorDocument.vue';
 </script>
 
 <template>
-  <CKEditorClassicVue />
+  <!-- <CKEditorClassicVue /> -->
+  <CKEditorDocumentVue />
 </template>
